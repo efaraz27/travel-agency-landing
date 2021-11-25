7cb50149e5f3a27f13c45de8894132ffe99fb48e
efaraz27 <efaraz27@gmail.com>
2021-11-25 18:35:36 +0530

    implemented hero section

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from "../assets/Logo.png";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="navlinks">
+        <div className="navlink"><span>Destinations</span></div>
+        <div className="navlink"><span>Hotels</span></div>
+        <div className="navlink"><span>Flights</span></div>
+        <div className="navlink"><span>Bookings</span></div>
+        <div className="navlink"><span>Login</span></div>
+        <div className="navlink signup">Sign up</div>
+        <div className="navlink"><span>EN</span></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/sections/Hero.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import blob from "../assets/blob.svg";
+import heroImage from "../assets/hero-image.png";
+import underline from "../assets/underline.png";
+import playButton from "../assets/play-button.png";
+import "../styles/Hero.css";
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <img className="blob" src={blob} alt="blob" />
+      <Navbar />
+      <div className="hero-contents">
+        <div className="description">
+          <div className="tagline">Best Destinations around the world</div>
+          <div className="heading">
+            Travel, <span className="enjoy">enjoy</span>{" "}
+            <img className="underline" src={underline} alt="underline" /> and
+            live a new and full life
+          </div>
+          <div className="desc">
+            Built Wicket longer admire do barton vanity itself do in it.
+            Preferred to sportsmen it engrossed listening. Park gate sell they
+            west hard for the.
+          </div>
+          <div className="buttons">
+            <div className="yellow-btn">Find out more</div>
+            <div className="play-button">
+              <img src={playButton} alt="play" />
+              Play Demo
+            </div>
+          </div>
+        </div>
+        <img className="hero-image" src={heroImage} alt="Hero img" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
